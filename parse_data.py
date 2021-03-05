3e2db295d764a71b70c5f4d75ae0a7bfcc75212a
jhille <jhille@student.codam.nl>
2021-03-05 16:31:22 +0000

    working graph

--- parse_data.py
+++ parse_data.py
@@ -1,7 +1,7 @@
 import csv
 import numpy as np
 from sklearn.linear_model import LinearRegression
-
+import matplotlib.pyplot as plt
 #class country:
 #def __init__(self, namecountry, name, mcount, fcount, nacount, totalcount, ppp):
 #	self.name 
@@ -11,34 +11,78 @@ from sklearn.linear_model import LinearRegression
 #	self.totalcount = totalcount
 #	self.ppp = ppp
 
-def get_age(agestr):
-	if agestr == '18 - 24 years old':
-		return 1
-	if agestr == '25 - 34 years old':
-		return 2
-	if agestr == '35 - 44 years old':
-		return 3
-	if agestr == '45 - 54 years old':
-		return 4
-	if agestr == '55 - 64 years old':
-		return 5
-	if agestr == '65 years or older':
-		return 6
+#def get_age(agestr):
+#	if agestr == '18 - 24 years old':
+#		return 21
+#	if agestr == '25 - 34 years old':
+#		return 29.5
+#	if agestr == '35 - 44 years old':
+#		return 39.5
+#	if agestr == '45 - 54 years old':
+#		return 49.5
+#	if agestr == '55 - 64 years old':
+#		return 59.5
+#	if agestr == '65 years or older':
+#		return 70
 #___________________________________________
 
+def get_exp(expstr):
+	if expstr == '0-2 years':
+		return 1
+	if expstr == '3-5 years':
+		return 5
+	if expstr == '6-8 years':
+		return 7
+	if expstr == '9-11 years':
+		return 10
+	if expstr == '12-14 years':
+		return 13
+	if expstr == '15-17 years':
+		return 16
+	if expstr == '18-20 years':
+		return 19
+	if expstr == '21-23 years':
+		return 22
+	if expstr == '24-26 years':
+		return 25
+	if expstr == '27-29 years':
+		return 28
+	else:
+		return 0
+
+def outlier(salary):
+	if salary < '10' and salary > '0':
+		return 0
+	else:
+		return 1
 
 with open ('ppp.csv', 'r') as csv_file:
 	reader = csv.reader(csv_file, delimiter=",")
 	
 	i = 0
-	xfem = np.empty(10000)
-	yfem = np.empty(10000)
+	xfem = np.empty(100000)
+	yfem = np.empty(100000)
+
+	xman = np.empty(100000)
+	yman = np.empty(100000)
+
+
 	for row in reader:
-		if row[11] == 'Female' and row[15] != 'NA' and row[15] != 'Under 18 years old':
-			yfem.fill(row[17])
-			xfem.fill(get_age(row[15]))
-			#print(xfem[i], yfem[i])
-			i += 1
+		if row[11] == 'Female' and row[6] != 'NA' and row[6] != '30 or more years' and row[17] != 'NA' and outlier(row[17]) == 0:
+			yfem[i] = row[17]
+			xfem[i] = get_exp(row[6])
+			print(xfem[i])
+		if row[11] == 'Male' and row[6] != 'NA' and row[6] != '30 or more years':
+			xman = row[17]
+			yman = get_exp(row[6])
+		i += 1
+
+	#print(xfem[41])
+	plt.scatter(xfem, yfem)
+	plt.show()
+	#plt.scatter(xman, yman)
+	#plt.show()
+
 	xfem = xfem.reshape((-1, 1))
 	model = LinearRegression().fit(xfem, yfem)
 	print(model.intercept_)
