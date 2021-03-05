fd729ad20531f2d33d5ebf3dbef1412cffd4ee13
jhille <jhille@student.codam.nl>
2021-03-05 11:37:54 +0000

    squashed commits

--- parse_data.py
+++ parse_data.py
@@ -1,6 +1,6 @@
 import csv
 
-with open ('/home/himak/Downloads/dataset.csv', 'r') as csv_file:
+with open ('dataset.csv', 'r') as csv_file:
 	reader = csv.reader(csv_file, delimiter=",")
 	#row = list(reader)
 	header = next(reader)
@@ -12,12 +12,13 @@ with open ('/home/himak/Downloads/dataset.csv', 'r') as csv_file:
 	
 	for row in reader:
 		if row[3] == 'United States':
+			print(row[56])
 			us_count += 1
 			if row[56] == 'Female':
 				female_count += 1
 			elif row[56] == 'Male':
 				male_count += 1
-	
 print(us_count)
-print(female_count)
+print("number of female employees in the US:", female_count)
+print("number of male employees in the US:", male_count)
 	
